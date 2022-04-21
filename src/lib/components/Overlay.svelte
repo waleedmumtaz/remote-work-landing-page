1d01d9924f10f8c925ee1e08e565dc4ee5e9b36d
Waleed <waleedthedev@gmail.com>
2022-04-22 03:49:27 +0500

    Add overlay

---
+++ src/lib/components/Overlay.svelte
@@ -0,0 +1,15 @@
+<div class="overlay">
+	<slot />
+</div>
+
+<style>
+	.overlay {
+		min-width: 100vh;
+		min-height: 100vh;
+		position: fixed;
+		inset: 0;
+		background-color: black;
+		opacity: 0.5;
+		z-index: 10;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,13 @@
 <script>
 	import Header from '$lib/components/Header.svelte';
 	import Main from '$lib/components/Main.svelte';
+	import Overlay from '$lib/components/Overlay.svelte';
+	import { isOverlayOpen } from '$lib/stores/OverlayStore.js';
 	import '../styles/global.css';
 </script>
 
+{#if $isOverlayOpen}
+	<Overlay />
+{/if}
 <Header />
 <Main />
